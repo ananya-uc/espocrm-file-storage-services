ef05b80a8824d362469a97c3d5c933b5baae9022
Karthik Bhat K <thebuzzycoder@gmail.com>
2020-02-06 01:36:52 +0530

    dev started

---
+++ tests/unit/InitTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests\unit;
+
+use PHPUnit\Framework\TestCase;
+
+class InitTest extends TestCase
+{
+    public function setUp(): void
+    {
+    }
+
+    public function tearDown(): void
+    {
+    }
+
+    /**
+     * @test
+     */
+    public function trueThat()
+    {
+        $this->assertTrue(true);
+    }
+}
